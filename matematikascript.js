bd69f26f0e26c8f5b9add3835bdf49e25866834d
Linda <banjaninborna@gmail.com>
2019-07-18 15:27:40 +0200

    gotovo, valjda

---
+++ matematikascript.js
@@ -0,0 +1,66 @@
+function onSumbit(event) {
+  var x1 = document.getElementById("x1").value;
+  var y1 = document.getElementById("y1").value;
+  var result = "";
+  event.preventDefault();
+  var x1 = document.getElementById("x1").value;
+  var y1 = document.getElementById("y1").value;
+  var x2 = document.getElementById("x2").value;
+  var y2 = document.getElementById("y2").value;
+  var x3 = document.getElementById("x3").value;
+  var y3 = document.getElementById("y3").value;
+  var x_1 = x2 - x1;
+  var y_1 = y2 - y1;
+  var x_2 = x3 - x2;
+  var y_2 = y3 - y2;
+  var l1 = Math.sqrt(x_1 * x_1 + y_1 * y_1);
+  var l2 = Math.sqrt(x_2 * x_2 + y_2 * y_2);
+  var k1 = y_1 / x_1;
+  var k2 = y_2 / x_2;
+
+  function angle(k) {
+    if (k === Infinity) {
+      return 90;
+    } else if (k === -Infinity) {
+      return 270;
+    } else {
+      return (Math.atan(k) * 180) / Math.PI;
+    }
+  }
+
+  if (angle(k1) === angle(k2)) {
+    kut = 0;
+  } else if (angle(k1) > angle(k2)) {
+    kut = angle(k1) - angle(k2);
+  } else {
+    kut = angle(k2) - angle(k1);
+  }
+
+  if (kut > 180) {
+    kut -= 360;
+  }
+  var result1 = "";
+  var result2 = "";
+  var result3 = "";
+
+  result1 += l1;
+  result2 += l2;
+  result3 += kut;
+  document.getElementById("result1").innerHTML = result1;
+  document.getElementById("result2").innerHTML = result2;
+  document.getElementById("result3").innerHTML = result3;
+}
+function onReset(event) {
+  event.preventDefault();
+  x1.innerText = 0;
+  y1.innerText = 0;
+  x2.innerText = 0;
+  y2.innerText = 0;
+  x3.innerText = 0;
+  y3.innerText = 0;
+  result1.innerText = "";
+  result2.innerText = "";
+  result3.innerText = "";
+}
+document.querySelector("form").onsubmit = onSumbit;
+document.querySelector("form").onreset = onReset;
