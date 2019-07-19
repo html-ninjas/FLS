90de2b3085f5d02d99775e2ab1a197aed749ea0e
Linda <banjaninborna@gmail.com>
2019-07-19 12:38:28 +0200

    gotovo

--- matematikascript.js
+++ matematikascript.js
@@ -1,65 +1,93 @@
+var j = 0;
+var x;
+var y;
+var xco = [];
+var yco = [];
+
 function onSumbit(event) {
-  var x1 = document.getElementById("x1").value;
-  var y1 = document.getElementById("y1").value;
   event.preventDefault();
-  var x1 = document.getElementById("x1").value;
-  var y1 = document.getElementById("y1").value;
-  var x2 = document.getElementById("x2").value;
-  var y2 = document.getElementById("y2").value;
-  var x3 = document.getElementById("x3").value;
-  var y3 = document.getElementById("y3").value;
-  var x_1 = x2 - x1;
-  var y_1 = y2 - y1;
-  var x_2 = x3 - x2;
-  var y_2 = y3 - y2;
-  var l1 = Math.sqrt(x_1 * x_1 + y_1 * y_1);
-  var l2 = Math.sqrt(x_2 * x_2 + y_2 * y_2);
-  var k1 = y_1 / x_1;
-  var k2 = y_2 / x_2;
+  j += 1;
 
-  function angle(k) {
-    if (k === Infinity) {
-      return 90;
-    } else if (k === -Infinity) {
-      return 270;
-    } else {
-      return (Math.atan(k) * 180) / Math.PI;
+  x = document.getElementById("x1").value;
+  y = document.getElementById("y1").value;
+
+  document.getElementById("x1").value = "";
+  document.getElementById("y1").value = "";
+
+  xco.push(x);
+  yco.push(y);
+
+  function length(x1, y1, x2, y2) {
+    var x_1 = x2 - x1;
+    var y_1 = y2 - y1;
+    var l1 = Math.sqrt(x_1 * x_1 + y_1 * y_1);
+    return l1;
+  }
+
+  function trig(x1, y1, x2, y2, x3, y3) {
+    var x_1 = x2 - x1;
+    var y_1 = y2 - y1;
+    var x_2 = x3 - x2;
+    var y_2 = y3 - y2;
+    var k1 = y_1 / x_1;
+    var k2 = y_2 / x_2;
+    function angle(k) {
+      if (k === Infinity) {
+        return 90;
+      } else if (k === -Infinity) {
+        return 270;
+      } else {
+        return (Math.atan(k) * 180) / Math.PI;
+      }
     }
+    if (angle(k1) === angle(k2)) {
+      kut = 0;
+    } else if (angle(k1) > angle(k2)) {
+      kut = angle(k1) - angle(k2);
+    } else {
+      kut = angle(k2) - angle(k1);
+    }
+
+    if (kut > 180) {
+      kut = Math.abs(kut - 360);
+    }
+    return kut;
   }
 
-  if (angle(k1) === angle(k2)) {
-    kut = 0;
-  } else if (angle(k1) > angle(k2)) {
-    kut = angle(k1) - angle(k2);
-  } else {
-    kut = angle(k2) - angle(k1);
+  if (j == 1) {
   }
-
-  if (kut > 180) {
-    kut -= 360;
+  if (j == 2) {
+    var node1 = document.createElement("li");
+    var textnode1 = document.createTextNode(
+      length(xco[0], yco[0], xco[1], yco[1])
+    );
+    node1.appendChild(textnode1);
+    document.getElementById("list").appendChild(node1);
   }
-  var result1 = "";
-  var result2 = "";
-  var result3 = "";
+  if (j > 2) {
+    var node2 = document.createElement("li");
+    var textnode2 = document.createTextNode(
+      -1 *
+        trig(
+          xco[j - 3],
+          yco[j - 3],
+          xco[j - 2],
+          yco[j - 2],
+          xco[j - 1],
+          yco[j - 1]
+        )
+    );
+    node2.appendChild(textnode2);
+    document.getElementById("list").appendChild(node2);
 
-  result1 += l1;
-  result2 += l2;
-  result3 += kut;
-  document.getElementById("result1").innerHTML = result1;
-  document.getElementById("result2").innerHTML = result2;
-  document.getElementById("result3").innerHTML = result3;
+    var node3 = document.createElement("li");
+    var textnode3 = document.createTextNode(
+      length(xco[j - 2], yco[j - 2], xco[j - 1], yco[j - 1])
+    );
+    node3.appendChild(textnode3);
+    document.getElementById("list").appendChild(node3);
+  }
 }
-function onReset(event) {
-  event.preventDefault();
-  x1.innerText = 0;
-  y1.innerText = 0;
-  x2.innerText = 0;
-  y2.innerText = 0;
-  x3.innerText = 0;
-  y3.innerText = 0;
-  result1.innerText = "";
-  result2.innerText = "";
-  result3.innerText = "";
-}
+document.getElementById("x1").value = "";
+document.getElementById("y1").value = "";
 document.querySelector("form").onsubmit = onSumbit;
-document.querySelector("form").onreset = onReset;
