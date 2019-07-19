a9baa9d14c5fd63014547cce9efc736a4c6792f3
Lukas Miholic <lukas.miholiclol@gmail.com>
2019-07-19 12:40:56 +0200

    start

--- matematikascript.js
+++ matematikascript.js
@@ -63,3 +63,19 @@ function onReset(event) {
 }
 document.querySelector("form").onsubmit = onSumbit;
 document.querySelector("form").onreset = onReset;
+
+function downloadFile(filename, text) {
+  var element = document.createElement("a");
+  element.setAttribute(
+    "href",
+    "data:text/plain;charset=utf-8," + encodeURIComponent(text)
+  );
+  element.setAttribute("download", filename);
+
+  element.style.display = "none";
+  document.body.appendChild(element);
+
+  element.click();
+
+  document.body.removeChild(element);
+}
