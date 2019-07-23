0d8f23479ce131e3effb6ca59e96c40f790b29b9
Jerko Gunjača <jerkangg@gmail.com>
2019-07-23 12:50:46 +0200

    Action point & add action button

--- FLS_js_file.js
+++ FLS_js_file.js
@@ -1,3 +1,9 @@
+function addAction(points) {
+  var lastPoint = points[points.length - 1];
+  lastPoint.type = "action";
+  redraw(ctx, img, points);
+}
+
 function clearCanvas(canvasContext, img) {
   canvasContext.clearRect(0, 0, 800, 400);
   canvasContext.globalAlpha = 0.5;
@@ -13,9 +19,9 @@ function addLiElement(listId, x, y) {
   getOrderedList.appendChild(newLi);
 }
 
-function drawCircle(canvasContext, point) {
+function drawCircle(canvasContext, point, color) {
   canvasContext.beginPath();
-  canvasContext.fillStyle = "yellow";
+  canvasContext.fillStyle = color;
   canvasContext.strokeStyle = "black";
   canvasContext.lineWidth = 2;
   canvasContext.arc(
@@ -31,29 +37,33 @@ function drawCircle(canvasContext, point) {
 
 function drawPoints(canvasContext, points) {
   for (var i = 0; i < points.length; i++) {
-    drawCircle(canvasContext, points[i]);
+    if (points[i].type === "action") {
+      drawCircle(canvasContext, points[i], "#00cd00");
+    } else {
+      drawCircle(canvasContext, points[i], "yellow");
+    }
   }
 }
 
 function drawLine(canvasContext, pointA, pointB) {
   if (pointB.direction === "forwards") {
-    canvasContext.beginPath();
-    canvasContext.strokeStyle = "red";
-    canvasContext.lineWidth = 5;
-
-    canvasContext.moveTo(pointA.coordinates[0], pointA.coordinates[1]);
-    canvasContext.lineTo(pointB.coordinates[0], pointB.coordinates[1]);
-    canvasContext.stroke();
+    colorLine(canvasContext, pointA, pointB, "#831100", 7);
+    colorLine(canvasContext, pointA, pointB, "#FF2600", 3);
   } else {
-    canvasContext.beginPath();
-    canvasContext.strokeStyle = "blue";
-    canvasContext.lineWidth = 5;
-
-    canvasContext.moveTo(pointA.coordinates[0], pointA.coordinates[1]);
-    canvasContext.lineTo(pointB.coordinates[0], pointB.coordinates[1]);
-    canvasContext.stroke();
+    colorLine(canvasContext, pointA, pointB, "#002E7A", 7);
+    colorLine(canvasContext, pointA, pointB, "#0433FF", 3);
   }
 }
+
+function colorLine(canvasContext, pointA, pointB, color, width) {
+  canvasContext.beginPath();
+  canvasContext.strokeStyle = color;
+  canvasContext.lineWidth = width;
+  canvasContext.moveTo(pointA.coordinates[0], pointA.coordinates[1]);
+  canvasContext.lineTo(pointB.coordinates[0], pointB.coordinates[1]);
+  canvasContext.stroke();
+}
+
 function drawLines(canvasContext, points) {
   for (var i = 0; i < points.length - 1; i++) {
     drawLine(canvasContext, points[i], points[i + 1]);
@@ -88,11 +98,19 @@ function onCanvasClick(event) {
   y -= canvasDivisor.offsetTop;
 
   if (event.shiftKey) {
-    points.push({ coordinates: [x, y], direction: "backwards" });
+    points.push({
+      coordinates: [x, y],
+      direction: "backwards",
+      type: "waypoint"
+    });
     console.log(points);
     console.log("Shift");
   } else {
-    points.push({ coordinates: [x, y], direction: "forwards" });
+    points.push({
+      coordinates: [x, y],
+      direction: "forwards",
+      type: "waypoint"
+    });
     console.log("No shift");
   }
 
@@ -127,6 +145,11 @@ function update(points) {
     document.getElementById("clearPath").setAttribute("disabled", "");
   } else {
     document.getElementById("clearPath").removeAttribute("disabled");
+  }
+  if (points.length === 0) {
+    document.getElementById("addAction").setAttribute("disabled", "");
+  } else {
+    document.getElementById("addAction").removeAttribute("disabled");
   }
 }
 
